872c4595b6957bc81a4466e20cc45c5af875e8a8
ashvani <dubey.ashvanikumar@gmail.com>
2024-02-18 00:13:17 +0530

    Updated hmm struct

--- src/hmm.rs
+++ src/hmm.rs
@@ -3,27 +3,30 @@
 /// HMM with A (transition probability matrix N x N), B (emission probability matrix N x T)
 /// N as number of hidden states, M is number of unique observations and T is length of observations
 /// pie as initial probabilities
+/// O is observation sequence
 
 
-struct Hmm {
-    A: Vec<Vec<f32>>,
-    B: Vec<Vec<f32>>,
-    pie: Vec<f32>,
-    alpha: Vec<Vec<f32>>,
-    beta: Vec<Vec<f32>>,
-    N: u32,
-    T: u32,
-    M: u32
+pub struct Hmm {
+    pub a: Vec<Vec<f32>>, // N by N 
+    pub b: Vec<Vec<f32>>, // N by M 
+    pub o: Vec<usize>, // T 
+    pub pie: Vec<f32>, //N 
+    pub alpha: Vec<Vec<f32>>, // T by N  
+    pub beta: Vec<Vec<f32>>, // N by M
+    pub c: Vec<f32>, // T 
+    pub n: usize,
+    pub t: usize,
+    pub m: usize 
 }
 
 impl Hmm {
 
-    fn read_A() {
+    fn read_a() {
         // read transition probabilities from some file
         todo!()
     }
 
-    fn read_B() {
+    fn read_b() {
         // read emission probabilities from some file
         todo!()
     }
@@ -35,14 +38,54 @@ impl Hmm {
     }
 
 
-    fn forward(&mut self) {
-        for i in 0..N {
-            alpha[i][0] = pie[i] * beta[i] * 
-
-        todo!()
+    fn loglikelihood(&mut self) -> f32 {
+        -1.0 * self.c.iter().map(|x| f32::log10(*x)).sum::<f32>()
     }
 
 
+    fn backward(&mut self) {
+
+    }
+
+
+    pub fn forward(&mut self) {
+
+        let mut sum = 0.0;
+        for i in 0..self.n {
+            self.alpha[0][i] = self.pie[i] * self.b[i][self.o[0]];
+            sum += self.alpha[0][i];
+        }
+
+        self.c.push(1.0 / sum);
+
+        for i in 0..self.n {
+            self.alpha[i][0] *=  self.c[0];
+        }
+
+        for t in 1..self.t {
+            let mut sum = 0.0;
+            for i in 0..self.n {
+                let mut val = 0.0;
+                for j in 0..self.n {
+                    val += self.alpha[j][t-1] * self.a[j][i];
+                }
+                self.alpha[i][t] = val * self.b[i][self.o[t]];
+                sum += self.alpha[i][t];
+            }
+
+            self.c.push(1.0 / sum);
+            for i in 0..self.n {
+                self.alpha[i][t] *= self.c[t];
+            }
+
+        }
+            
+    }
+
+
+    fn train(&mut self) {
+
+    }
 
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,48 @@
+use grid::*;
+mod hmm;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut grid_a = grid![[1, 2, 3]
+        [4, 5, 6]
+        [7, 8, 9]];
+    let grid_b = grid![[10, 11, 12]
+        [13, 14, 15]
+        [16, 17, 18]];
+
+    for ((i, j), val) in grid_a.indexed_iter_mut() {
+        println!("value at {} and {} is {}", i, j, val);
+    }
+
+
+    println!("sum of elements in grid a: {}", grid_a.iter().sum::<i32>());
+
+    for ((i, j), val) in grid_a.indexed_iter_mut() {
+        *val *= 3;
+        println!("value at {} and {} is {}", i, j, val);
+    }
+    println!("{:?}", grid_a);
+
+    let result:i32 = grid_a.iter().sum();
+    println!("{}", result);
+    let val: Vec<f32> = vec![10.0, 3.5, 10.23];
+    println!("{:?}", val.iter().map(|x| f32::log10(*x)).sum::<f32>());
+
+
+    let val = vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]];
+    println!("{:?}", val);
+    println!("{}", val[1][2]);
+
+    let a = vec![vec![0.7, 0.3], vec![0.4, 0.6]];
+    let b = vec![vec![0.1, 0.4, 0.5], vec![0.7, 0.2, 0.1]];
+    let alpha = vec![];
+    let beta = vec![];
+    let o = vec![0, 1, 0, 2];
+    let mut model = hmm::Hmm{a,
+    b, alpha, beta, o, c: vec![],
+    t: 4, n: 2, m: 3, pie: vec![0.0, 1.0]
+    };
+
+    model.forward();
 }
 
