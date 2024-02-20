385ed680bf2cbba8807aa25dd56e9046388b4c78
ashvani <dubey.ashvanikumar@gmail.com>
2024-02-20 23:56:31 +0530

    Added builder pattern for struct

--- src/hmm.rs
+++ src/hmm.rs
@@ -4,39 +4,47 @@
 /// N as number of hidden states, M is number of unique observations and T is length of observations
 /// pie as initial probabilities
 /// O is observation sequence
+/// model = Hmm::new{n, m, t};
+/// model.set_a();
+/// model.set_pie();
+/// model.set_b();
+/// model.initialize_a();
+/// model.initialize_b();
+/// model.initialize_pie();
+use derive_builder::Builder;
 
-
+#[derive(Builder)]
 pub struct Hmm {
-    pub a: Vec<Vec<f32>>, // N by N 
-    pub b: Vec<Vec<f32>>, // N by M 
-    pub o: Vec<usize>, // T 
-    pub pie: Vec<f32>, //N 
-    pub alpha: Vec<Vec<f32>>, // T by N  
-    pub beta: Vec<Vec<f32>>, // T by N  
-    pub c: Vec<f32>, // T 
-    pub n: usize,
-    pub t: usize,
-    pub m: usize 
+    emission_prob: Vec<Vec<f32>>, // N by N 
+    transition_prob: Vec<Vec<f32>>, // N by M 
+    observations: Vec<usize>, // T 
+    pie: Vec<f32>, //N 
+    alpha: Vec<Vec<f32>>, // T by N  
+    beta: Vec<Vec<f32>>, // T by N  
+    c: Vec<f32>, // T 
+    n: usize,
+    t: usize,
+    m: usize 
 }
 
 impl Hmm {
 
-    fn read_a() {
-        // read transition probabilities from some file
-        todo!()
-    }
-
-    fn read_b() {
-        // read emission probabilities from some file
-        todo!()
-    }
-
-
-    fn initialize() {
-        todo!()
-
-    }
-
+//    fn read_a() {
+//        // read transition probabilities from some file
+//        todo!()
+//    }
+//
+//    fn read_b() {
+//        // read emission probabilities from some file
+//        todo!()
+//    }
+//
+//
+//    fn initialize() {
+//        todo!()
+//
+//    }
+//
 
 
 
@@ -60,7 +68,7 @@ impl Hmm {
             for i in 0..self.n {
                 let mut val:f32 = 0.0;
                 for j in 0..self.n {
-                    val += self.a[i][j] * self.b[j][self.o[t+1]] * self.beta[t + 1][j]
+                    val += self.transition_prob[i][j] * self.emission_prob[j][self.observations[t+1]] * self.beta[t + 1][j]
                 }
                 val *= self.c[t];
                 beta_row.push(val);
@@ -76,7 +84,7 @@ impl Hmm {
         let mut sum = 0.0;
         let mut alpha_row = vec![];
         for i in 0..self.n {
-            alpha_row.push(self.pie[i] * self.b[i][self.o[0]]);
+            alpha_row.push(self.pie[i] * self.emission_prob[i][self.observations[0]]);
             sum += alpha_row[i];
         }
 
@@ -92,9 +100,9 @@ impl Hmm {
             for i in 0..self.n {
                 let mut val = 0.0;
                 for j in 0..self.n {
-                    val += self.alpha[t-1][j] * self.a[j][i];
+                    val += self.alpha[t-1][j] * self.transition_prob[j][i];
                 }
-                alpha_row.push(val * self.b[i][self.o[t]]);
+                alpha_row.push(val * self.emission_prob[i][self.observations[t]]);
                 sum += alpha_row[i];
             }
 
@@ -115,13 +123,12 @@ impl Hmm {
             for j in 0..self.n {
                 let mut val:f32 = 0.0;
                 for t in 0..self.t-1 {
-                    val += self.alpha[t][i] * self.a[i][j] * self.b[j][self.o[t+1]] * self.beta[t+1][j]
+                    val += self.alpha[t][i] * self.transition_prob[i][j] * self.emission_prob[j][self.observations[t+1]] * self.beta[t+1][j]
                 }
                 row.push(val);
             }
             xi.push(row);
         }
-        println!("matrix xi = {:?}", &xi);
 
         let mut gamma: Vec<Vec<f32>> = vec![];
         for t in 0..self.t-1 {
@@ -129,14 +136,13 @@ impl Hmm {
             for i in 0..self.n {
                 let mut val:f32 = 0.0;
                 for j in 0..self.n {
-                    val += self.alpha[t][i] * self.a[i][j] * self.b[j][self.o[t+1]] * self.beta[t+1][j];
+                    val += self.alpha[t][i] * self.transition_prob[i][j] * self.emission_prob[j][self.observations[t+1]] * self.beta[t+1][j];
                 }
                 row.push(val);
             }
             gamma.push(row);
         }
         gamma.push(self.alpha[self.t - 1].to_owned());
-        println!("matrix gamma = {:?}", &gamma);
         self.pie = gamma[0].to_owned();
 
         let mut gamma_t_1: Vec<f32> = vec![];
@@ -148,14 +154,14 @@ impl Hmm {
             gamma_t_1.push(val);
         }
 
-               // re-estimate a[i][j]
+        println!("matrix a = {:?}", self.transition_prob);
+         // re-estimate a[i][j]
         for i in 0..self.n {
             for j in 0..self.n {
-                self.a[i][j] = xi[i][j] / gamma_t_1[i];
+                self.transition_prob[i][j] = xi[i][j] / gamma_t_1[i];
             }
         }
 
-        println!("matrix b = {:?}", self.b);
         // re-estimate b[j][k]
         for i in 0..self.n {
             let mut denom: f32 = 0.0;
@@ -166,20 +172,24 @@ impl Hmm {
             for j in 0..self.m {
                 let mut numer: f32 = 0.0;
                 for t in 0..self.t {
-                    if self.o[t] == j {
+                    if self.observations[t] == j {
                         numer += gamma[t][i];
                     }
                 }
-                self.b[i][j] = numer / denom;
+                self.emission_prob[i][j] = numer / denom;
             }
         }
 
-        println!("matrix b = {:?}", self.b);
+        println!("matrix b = {:?}", self.emission_prob);
     }
 
     pub fn train(&mut self) {
-        self.estimate();
-
+        for _i in 0..100{
+            self.forward();
+            self.backward();
+            self.estimate();
+            println!("loglikelihood = {}", self.loglikelihood());
+        }
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,6 @@
-use grid::*;
 mod hmm;
 
 fn main() {
-
-    let mut grid_a = grid![[1, 2, 3]
-        [4, 5, 6]
-        [7, 8, 9]];
-    let grid_b = grid![[10, 11, 12]
-        [13, 14, 15]
-        [16, 17, 18]];
-
-    for ((i, j), val) in grid_a.indexed_iter_mut() {
-        println!("value at {} and {} is {}", i, j, val);
-    }
-
-
-    println!("sum of elements in grid a: {}", grid_a.iter().sum::<i32>());
-
-    for ((i, j), val) in grid_a.indexed_iter_mut() {
-        *val *= 3;
-        println!("value at {} and {} is {}", i, j, val);
-    }
-    println!("{:?}", grid_a);
-
-    let result:i32 = grid_a.iter().sum();
-    println!("{}", result);
-    let val: Vec<f32> = vec![10.0, 3.5, 10.23];
-    println!("{:?}", val.iter().map(|x| f32::log10(*x)).sum::<f32>());
-
-
-    let val = vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]];
-    println!("{:?}", val);
-    println!("{}", val[1][2]);
 
     let a = vec![vec![0.7, 0.3], vec![0.4, 0.6]];
     let b = vec![vec![0.1, 0.4, 0.5], vec![0.7, 0.2, 0.1]];
@@ -39,16 +8,32 @@ fn main() {
     let alpha = vec![];
     let beta = vec![];
     let o = vec![0, 1, 0, 2];
-    let mut model = hmm::Hmm{a,
-    b, alpha, beta, o, c: vec![],
-    t: 4, n: 2, m: 3, pie 
-    };
+    let mut model = hmm::HmmBuilder::default()
+        .observations(o)
+        .transition_prob(a)
+        .emission_prob(b)
+        .n(2)
+        .m(3)
+        .t(4)
+        .c(vec![])
+        .beta(beta)
+        .alpha(alpha)
+        .pie(pie)
+        .build()
+        .unwrap();
 
-    model.forward();
-    println!("log likelihood = {}", model.loglikelihood());
-    println!("alpha = {:?}", model.alpha);
-    model.backward();
-    println!("beta = {:?}", model.beta);
+//    let mut model = hmm::Hmm{a,
+//    b, alpha, beta, o, c: vec![],
+//    t: 4, n: 2, m: 3, pie 
+//    };
+//
+//    model.forward();
+//    println!("log likelihood = {}", model.loglikelihood());
+//    println!("alpha = {:?}", model.alpha);
+//    model.backward();
+//    println!("beta = {:?}", model.beta);
     model.train();
+//    model.forward();
+//    println!("log likelihood = {}", model.loglikelihood());
 }
 
